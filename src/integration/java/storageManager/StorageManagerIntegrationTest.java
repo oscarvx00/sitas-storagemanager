b61fd1f0ebed7f4f143acd2a98816bb1867738d0
oscarvx00 <oscarvicente@usal.es>
2022-09-17 21:04:34 +0200

    Removed dummy integration test

--- src/integration/java/storageManager/StorageManagerIntegrationTest.java
+++ src/integration/java/storageManager/StorageManagerIntegrationTest.java
@@ -5,10 +5,4 @@ import org.junit.jupiter.api.Test;
 
 public class StorageManagerIntegrationTest {
 
-    @Test
-    public void testDummy() {
-        StorageManager underTest = new StorageManager();
-        Assertions.assertEquals(0, underTest.dummy());
-    }
-
 }
