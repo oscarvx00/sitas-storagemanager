50ff6ae6b00d58ada182be1ce9af8861d4035486
oscarvx00 <oscarvicente@usal.es>
2022-09-17 20:55:35 +0200

    Remove dummy test

--- src/test/java/storageManager/StorageManagerTest.java
+++ src/test/java/storageManager/StorageManagerTest.java
@@ -5,11 +5,6 @@ import org.junit.jupiter.api.Test;
 
 public class StorageManagerTest {
 
-    @Test
-    public void testDummy(){
-        StorageManager underTest = new StorageManager();
 
-        Assertions.assertEquals(0, underTest.dummy());
-    }
 
 }
